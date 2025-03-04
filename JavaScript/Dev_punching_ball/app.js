eaaf15bea3bea29bdfc8b39e66edde39aab28294
HCGBD <mamadoudiallo34204@gmail.com>
2025-03-04 19:02:41 +0000

    Une petite v1.0 est faite d'abord

--- JavaScript/Dev_punching_ball/app.js
+++ JavaScript/Dev_punching_ball/app.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector("#canvas")
+const commencer=document.querySelector("#begin")
 const ctx=canvas.getContext("2d")
 
 canvas.width="1000"
@@ -7,29 +8,48 @@ canvas.height="600"
 let ball={
     x:canvas.width/2,
     y:canvas.height/2,
-    radius:30,
-    color:"red"
+    rayon:30,
+    couleur:"red",
+    vitesse:3
 }
+let dx=ball.vitesse
+let dy=ball.vitesse
 
 function drawBall(){
     ctx.beginPath()
-    ctx.arc(ball.x,ball.y,ball.radius,0,Math.PI*2)
-    ctx.fillStyle=ball.color
+    ctx.arc(ball.x,ball.y,ball.rayon,0,Math.PI*2)
+    ctx.fillStyle=ball.couleur
+    ctx.clearRect(0,0,canvas.width,canvas.height)
     ctx.fill()
     ctx.closePath()
 }
 drawBall()
 
-function moveBall(event){
-    const {offsetX:newX,offsetY:newY}=event
-    ball.x=newX
-    ball.y=newY
-   
+function moveBall(){
+    ball.x+=dx
+    ball.y+=dy
+    if(ball.x+ball.rayon>canvas.width || ball.x-ball.rayon<0){
+       dx=-dx
+      
+    }
+    if(ball.y+ball.rayon>canvas.height || ball.y-ball.rayon<0){
+        dy=-dy
+    }
+    
+
+ 
     drawBall()
 }
 
-function move_Left_Right(){
 
+function animate(){
+    moveBall()
+    requestAnimationFrame(animate)
 }
 
-canvas.addEventListener("mousemove",moveBall)
+
+
+// canvas.addEventListener("mousemove",moveBall)
+commencer.addEventListener("click",function(){
+    animate()
+})
